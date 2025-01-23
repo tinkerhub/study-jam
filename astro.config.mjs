3b4d7b0f597ef04f02c908a1cfab39eff2eb2af7
Abhiram N J <abhiramnj@gmail.com>
2025-01-23 15:16:37 +0530

    fix: netlify package issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
+// Import the Netlify adapter
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  site: "https://studyjams.tinkerub.org", // Update the site URL if needed
   integrations: [tailwind()],
-  output: 'server',
+  output: 'server', // Required for server-side adapters like Netlify
   adapter: netlify(),
 });
