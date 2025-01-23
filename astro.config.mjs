38b6c85386fc92d8c4446b57277f40d2c466c8df
Abhiram N J <abhiramnj@gmail.com>
2025-01-23 14:58:13 +0530

    fix: netlify render issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://positivustheme.vercel.app",
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
